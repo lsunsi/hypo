03fef570dd6fee546a5be0106f0cd6ff65be1e8e
Lucas Sunsi Abreu <lsunsi@proton.me>
2026-07-24 08:28:46 -0300

    trait/impl Render

---
+++ src/render.rs
@@ -0,0 +1,109 @@
+pub trait Render {
+    fn render(&self, to: &mut String);
+}
+
+impl Render for char {
+    fn render(&self, to: &mut String) {
+        to.push_str(&self.to_string());
+    }
+}
+
+impl Render for &str {
+    fn render(&self, to: &mut String) {
+        to.push_str(self);
+    }
+}
+
+impl Render for String {
+    fn render(&self, to: &mut String) {
+        to.push_str(self);
+    }
+}
+
+macro_rules! impl_for_numbers {
+    ($($number:ty)+) => {$(
+        impl Render for $number {
+            fn render(&self, to: &mut String) {
+                to.push_str(&self.to_string());
+            }
+        }
+    )*};
+}
+
+impl_for_numbers!(u8 i8 u16 i16 u32 i32 u64 i64 u128 i128 usize isize);
+
+impl<R: Render> Render for Option<R> {
+    fn render(&self, to: &mut String) {
+        if let Some(r) = self {
+            r.render(to);
+        }
+    }
+}
+
+impl<R1: Render, R2: Render> Render for Result<R1, R2> {
+    fn render(&self, to: &mut String) {
+        match self {
+            Ok(r) => r.render(to),
+            Err(r) => r.render(to),
+        }
+    }
+}
+
+impl<R: Render> Render for &[R] {
+    fn render(&self, to: &mut String) {
+        for r in self.iter() {
+            r.render(to);
+        }
+    }
+}
+
+impl<const N: usize, R: Render> Render for [R; N] {
+    fn render(&self, to: &mut String) {
+        for r in self {
+            r.render(to);
+        }
+    }
+}
+
+impl<R: Render> Render for Vec<R> {
+    fn render(&self, to: &mut String) {
+        for r in self {
+            r.render(to);
+        }
+    }
+}
+
+macro_rules! impl_for_tuples {
+    ($head:ident) => {
+        impl Render for () {
+            fn render(&self, _: &mut String) {}
+        }
+    };
+    ($head:ident $($tail:ident)+) => {
+        impl <$($tail: Render),*> Render for ($($tail),*,) {
+            fn render(&self, to: &mut String) {
+                #[allow(non_snake_case, reason = "macro")]
+                let ($($tail),*,) = self;
+                ($($tail.render(to)),*);
+            }
+        }
+
+        impl_for_tuples!($($tail)*);
+    };
+}
+
+impl_for_tuples!(I H G F E D C B A);
+
+pub struct Raw<T>(pub T);
+impl<T: AsRef<str>> Render for Raw<T> {
+    fn render(&self, to: &mut String) {
+        to.push_str(self.0.as_ref());
+    }
+}
+
+pub struct Fn<F>(pub F);
+impl<F: std::ops::Fn(&mut String)> Render for Fn<F> {
+    fn render(&self, to: &mut String) {
+        self.0(to);
+    }
+}
---
+++ tests/render.rs
@@ -0,0 +1,93 @@
+use hypo::Render;
+
+#[macro_export]
+macro_rules! assert_render {
+    ($hypo:expr, $ex:literal) => {{
+        let mut s = String::new();
+        $hypo.render(&mut s);
+        assert_eq!(s, $ex);
+    }};
+}
+
+#[test]
+fn char() {
+    assert_render!('!', "!");
+}
+
+#[test]
+fn str() {
+    assert_render!("oiblz", "oiblz");
+}
+
+#[test]
+fn string() {
+    assert_render!(String::from("oiblz"), "oiblz");
+}
+
+#[test]
+fn numbers() {
+    assert_render!(0u8, "0");
+    assert_render!(1i8, "1");
+    assert_render!(2u16, "2");
+    assert_render!(3i16, "3");
+    assert_render!(4u32, "4");
+    assert_render!(5i32, "5");
+    assert_render!(6u64, "6");
+    assert_render!(7i64, "7");
+    assert_render!(8u128, "8");
+    assert_render!(9i128, "9");
+    assert_render!(0usize, "0");
+    assert_render!(1isize, "1");
+}
+
+#[test]
+fn option() {
+    assert_render!(None::<&str>, "");
+    assert_render!(Some("oiblz"), "oiblz");
+}
+
+#[test]
+fn result() {
+    assert_render!(Err::<&str, _>("oi"), "oi");
+    assert_render!(Ok::<_, &str>("blz"), "blz");
+}
+
+#[test]
+fn slice() {
+    assert_render!(&["oi", "blz"] as &[_], "oiblz");
+}
+
+#[test]
+fn array() {
+    assert_render!(["oi", "blz"], "oiblz");
+}
+
+#[test]
+fn vec() {
+    assert_render!(vec!["oi", "blz"], "oiblz");
+}
+
+#[test]
+fn tuples() {
+    assert_render!((), "");
+    assert_render!(("0",), "0");
+    assert_render!(("0", "1"), "01");
+    assert_render!(("0", "1", "2"), "012");
+    assert_render!(("0", "1", "2", "3"), "0123");
+    assert_render!(("0", "1", "2", "3", "4"), "01234");
+    assert_render!(("0", "1", "2", "3", "4", "5"), "012345");
+    assert_render!(("0", "1", "2", "3", "4", "5", "6"), "0123456");
+    assert_render!(("0", "1", "2", "3", "4", "5", "6", "7"), "01234567");
+}
+
+#[test]
+fn raw() {
+    use hypo::Raw;
+    assert_render!(Raw("<p>oiblz</p>"), "<p>oiblz</p>");
+}
+
+#[test]
+fn r#fn() {
+    use hypo::Fn;
+    assert_render!(Fn(|s: &mut String| s.push_str("oiblz")), "oiblz");
+}
